60f22da80918990ab0ff335b42bbfa33d4de5c9c
Ricardinho <ricardogonzaga98@gmail.com>
2025-04-08 22:06:39 -0300

    calendario com varias cores estilizados

--- main.js
+++ main.js
@@ -1,61 +1,58 @@
-function colorirDia() {
-    // Pegar valor de campo de dia
-    let days = document.getElementById('days').value;
+function colorirDia(){
 
-    // Pega o valor do campo de cor
-let color = document.getElementById('color'). 
-value;
+    //pegar valor de campo de dia
+    let days = document.getElementById('day').value;
+    //pegar valor do campor de cor
+    let color = document.getElementById('color').value;
+    //pega os elementos da tabela calendario
+    let calendar = document.getElementById('calendar');
 
-// Pega os elementos da tabela calendario
-let calendar = document.getElementById
-('calendar');
+    //verifica se o campo de dia foi preenchido
+    if(!days){
 
-// Verifica se o campo de dia foi prenchido
-if(!days){
-    alert('Favor informar o dia');
-} else if((days > 0) && (days < 31)){
-    // Se o numero de dias está no intervalo
-    //aplicará a cor na célula do calendario
-    let td = calendar.getElementsByTagName
-    ('td');
-    td.style.backgroundColor = color;
-} else {
-    alert("Favor informar um dia do calendario");
-}
+        alert('favor informar o dia!');
+   
+    }else if((days >0) && (days < 31)){
+       
+            //se o numero de dias esta no intervalo 
+            //aplicara a cor na celula do calendario 
+            let td = calendar.getElementsByTagName('td')[parseInt(days)+1];
+            td.style.backgroundColor = color;
 
-// Guarda a celula da tabela
-let elementos = document.querySelectorAll('td');
+    }else if(days > 31)
+    {
 
-// Declara a inicializa contadores
-let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+        alert('favor informar um dia valido!');
 
-// Loop para verificar o estilo de cada célula e contar quantas tem cor e qual cor
-for (let i = 0; i < elementos.length; i++ ){
-    // obtém o estilo computado do elemento atual
-    var estilo = window.getComputedStyle(elementos[i]);
-
-    // Obtem a cor de fundo atual da celula
-    var corEstilo = estilo.backgroundColor;
-    // Verifica a cor e incrementa contador
-    correspondente
-    if(corEstilo === "rgb(173,216,230)" || corEstilo === "lightBlue"){
-     contadorAzul ++;
-    } else if (corEstilo === "rgb(152, 215, 152)" || corEstilo === "palegreen"){
-        contadorVerde++;
-    } else if (corEstilo === "rgb(255, 182, 193)" || corEstilo === "lightPink"){
-        contadorRosa++;
-
-    } else if (corEstilo === "rgb(106, 90, 205)" || corEstilo === "slteBlue") {
-        contadorRoxo++;
-    }
-    
-    
     }
 
-    
+    //guarda  a celula da tabela
+    let elementos = document.querySelectorAll('td');
+    //declara e inicializa contadores
+    let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+
+    //loop para verificar o estilo de cada celula e contar quantas tem cor e qual cor 
+    for(let i = 0; i < elementos.length; i++){
+        
+        var estilo = window.getComputedStyle(elementos[i]);
+
+        //obtem a cor de fundo atual da celula
+        var corEstilo = estilo.backgroundColor;
+
+        //verifica a cor e incrementa contador correspondente
+        if(corEstilo === 'rgb(173, 216, 230)' || corEstilo === 'lightblue'){
+            contadorAzul++;
+        }else if(corEstilo === 'rgb(152, 215, 152)' || corEstilo === 'palegreen'){
+            contadorVerde++;
+        }else if(corEstilo === 'rgb(255, 182, 193)' || corEstilo === 'lightpink'){
+            contadorRosa++;
+        }else if(corEstilo === 'rgb(106, 90, 205)' || corEstilo === 'slateblue'){
+            contadorRoxo++;
+        }        
+
+        
+    }
+
 
 
 }
-
-
-
